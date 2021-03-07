28a5ca44d8822d98d2da8ad8aab2bfed3d98c5ee
Nolan Tait <nolanjtait@gmail.com>
2021-03-07 00:10:25 -0800

    Adds wizard controller

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Dropdown from './dropdown.js'
 import Pagination from './pagination.js'
+import Wizard from './wizard.js'
 
-
-export { Dropdown, Pagination }
+export { Dropdown, Pagination, Wizard }
