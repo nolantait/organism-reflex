9112d54c8638642c82c0f75d58905ec310c347d7
Nolan Tait <nolanjtait@gmail.com>
2021-03-09 03:04:58 -0800

    Adds collapsable controller

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import Pagination from './pagination.js'
 import Wizard from './wizard.js'
 import Selectable from './selectable.js'
 import Sortable from './sortable.js'
+import Collapsable from './collapsable.js'
 
-export { Dropdown, Pagination, Wizard, Selectable, Sortable }
+
+export { Dropdown, Pagination, Wizard, Selectable, Sortable, Collapsable }
